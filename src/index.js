058f6e2d7863113c0e6b8dd8f53717dd08982f0d
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-01-22 22:16:33 +0200

    assingment not yet complete

--- src/index.js
+++ src/index.js
@@ -1,90 +1,127 @@
-
-const maxValue = 3;
-const lowValue = 1;
+const maxValue = 100;
+const minValue = 0;
+const maxNumberOfTries = 10;
 let executionTime;
 
-let randomNumber = Math.floor(Math.random() * maxValue) + lowValue;
+//arvotaan random numero
+const random = () =>
+  Math.round(Math.random() * (maxValue - minValue)) + minValue;
+let randomNumber = random();
 
-const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-const time = document.querySelector('.time');
-const tries = document.querySelector('.tries');
+//haetaan html elementit
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const time = document.querySelector(".time");
+const tries = document.querySelector(".tries");
 
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessField = document.querySelector('.guessField');
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+const simulateBtn = document.querySelector(".startSimulate");
+const inputValue = document.getElementById("#inputNumber");
 
-let guessCount = 1;
+let guessHistory = [];
 let resetButton;
 
 const start = Date.now();
 
-
 const checkGuess = () => {
-
   const end = Date.now();
-  
+
   const userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    guesses.textContent = 'Previous guesses: ';
-  }
-  guesses.textContent += `${userGuess} `;
+  guessHistory.push(userGuess);
+  guesses.textContent = `Previous guesses: ${guessHistory}`;
 
   if (userGuess === randomNumber) {
-    lastResult.textContent = 'Congratulations! You got it right!';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    time.textContent = ` It took you ${(end - start)} milliseconds to find the right number.`;
-    tries.textContent = (`You quessed ${guessCount} times.`);
+    lastResult.textContent = "Congratulations! You got it right!";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = "";
+    time.textContent = ` It took you ${
+      end - start
+    } milliseconds to find the right number.`;
+    tries.textContent = `You quessed ${guessHistory.length} times.`;
     setGameOver();
-  } else if (guessCount === 10) {
-    lastResult.textContent = '!!!GAME OVER!!!';
-    lowOrHi.textContent = '';
+  } else if (guessHistory.length >= maxNumberOfTries) {
+    lastResult.textContent = "!!!GAME OVER!!!";
+    lowOrHi.textContent = "";
     setGameOver();
   } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
+    lastResult.textContent = "Wrong!";
+    lastResult.style.backgroundColor = "red";
     if (userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
+      lowOrHi.textContent = "Last guess was too low!";
     } else if (userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
+      lowOrHi.textContent = "Last guess was too high!";
     }
   }
 
-  guessCount++;
-  guessField.value = '';
   guessField.focus();
 };
 
-guessSubmit.addEventListener('click', checkGuess);
-
-
+guessSubmit.addEventListener("click", checkGuess);
 
 const setGameOver = (end) => {
   guessField.disabled = true;
   guessSubmit.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Start new game";
   document.body.append(resetButton);
-  resetButton.addEventListener('click', resetGame);
+  resetButton.addEventListener("click", resetGame);
 };
 
 const resetGame = () => {
   guessCount = 1;
 
-  const resetParas = document.querySelectorAll('.resultParas p');
+  const resetParas = document.querySelectorAll(".resultParas p");
   for (const resetPara of resetParas) {
-    resetPara.textContent = '';
+    resetPara.textContent = "";
   }
 
   resetButton.parentNode.removeChild(resetButton);
 
   guessField.disabled = false;
   guessSubmit.disabled = false;
-  guessField.value = '';
+  guessField.value = "";
   guessField.focus();
 
-  lastResult.style.backgroundColor = 'white';
-
-  randomNumber = Math.floor(Math.random() * maxValue) + lowValue;
+  /*  randomNumber = Math.floor(Math.random() * maxValue) + minValue; */
 };
+
+const runSimulation = () => {
+  let history = [];
+  let min = minValue;
+  let maxk = maxValue;
+
+  const run = (min, max) => {
+    if (history.length >= maxNumberOfTries) {
+      return;
+    }
+
+    const guess = Math.round((max - min) / 2) + min;
+    history.push(guess);
+
+    if (guess === randomNumber) {
+      return;
+    }
+    if (guess < randomNumber) {
+      return run(guess, max);
+    }
+    return run(min, guess);
+  };
+
+  run(minValue, maxValue);
+  return history;
+};
+
+const runSimulations = (times = 1) => {
+
+  randomNumber = random();
+  const history = runSimulation();
+  let sum = history.reduce((total, guess) => total + guess);
+  let average = sum / history.length;
+  console.log(
+    `Answer was '${randomNumber}. It took ${history.length} tries (${history} average number of total guess count ${average} )`
+  );
+};
+
+simulateBtn.addEventListener("click", runSimulations);
