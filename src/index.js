0a470add6abe6ddfb7014228c3c873dff83f5715
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-01-28 01:18:14 +0200

    almost everything from task1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ bearHello.src = "./assets/gifs/hello.gif";
 
 const cheatContainer = document.getElementById("cheat-container");
 let inputValue = document.querySelector("#inputValue");
+let coordinatesCntner = document.querySelector("#coordinates");
+let coordinates = document.createElement("p");
+let notifierContainer = document.querySelector("#notifier");
+let notifierText = document.createElement("p");
 
 let cheatArray = [];
 
@@ -24,4 +28,23 @@ const checkCheat = () => {
   }
 };
 
-document.addEventListener;
+//double click
+document.addEventListener("dblclick", (e) => {
+  console.log(e);
+  coordinatesCntner.append(coordinates);
+  coordinates.textContent = `you just clicked on these coordinates:  X ${e.screenX.toString()} and Y ${e.screenY.toString()}`;
+});
+
+//text hover
+cheatContainer.addEventListener("mouseenter", (e) => {
+  cheatContainer.style.color = "green";
+});
+cheatContainer.addEventListener("mouseleave", (e) => {
+  cheatContainer.style.color = "black";
+});
+
+let notifier = 0;
+const timer = setTimeout(() => {
+  notifierContainer.append(notifierText);
+  notifierText.textContent = `Hurry up you have been here browsing for 15 seconds already!!!!`;
+}, 15000);
