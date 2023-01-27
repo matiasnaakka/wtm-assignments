4ce0758efc8012dc578426d917aa478c67896019
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-01-28 00:25:30 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -1,73 +1,27 @@
-/* import { Sortable } from "@shopify/draggable";
+const bearHello = document.createElement("img");
+bearHello.src = "./assets/gifs/hello.gif";
 
-const sortable = new Sortable(document.querySelectorAll("ul"), {
-  draggable: "li",
+const cheatContainer = document.getElementById("cheat-container");
+let inputValue = document.querySelector("#inputValue");
+
+let cheatArray = [];
+
+inputValue.addEventListener("keydown", (e) => {
+  cheatArray.push(e.key);
+  checkCheat();
+  console.log(cheatArray);
 });
 
-const sortableUrl = document.querySelector("ul");
-
-let counter = 0;
-const timer = setInterval(() => {
-  counter++;
-  console.log(`2 sekunttia mennyt ${counter++} kerran`);
-  if (counter > 10) {
-    clearInterval(timer);
+const checkCheat = () => {
+  if (cheatArray.join("") === "hello") {
+    alert("Hello!");
+    cheatArray = [];
+    cheatContainer.append(bearHello);
+  } else if (cheatArray.length > 5) {
+    cheatArray = [];
+    location.reload();
+    alert("wrong cheatcode.");
   }
-}, 2000);
+};
 
-clearInterval(timer);
-console.log("Suoritus jatkuu");
-
-const divs = document.querySelectorAll("div");
-console.log(divs);
-
-divs[0].addEventListener("click", () => {
-  console.log("ekaa klikattu");
-});
-
-divs[1].addEventListener("click", () => {
-  console.log("tokaa klikattu");
-});
-
-divs[2].addEventListener("click", (event) => {
-  console.log;
-  console.log("kolmas klikattu", event);
-});
-
-divs[0].addEventListener("mouseenter", (event) => {
-  console.log("mouseenter", event);
-  divs[1].style.borderColor = "red";
-});
-
-divs[0].addEventListener("mouseleave", (event) => {
-  console.log("mouseleave", event);
-  divs[1].style.borderColor = "black";
-});
-
-document.addEventListener("keypress", (event) => {
-  console.log(event.key);
-  divs[2].click();
-
-  let inputField = document.querySelector("input");
-  inputField.addEventListener("focusout", (event) => {
-    console.log("input event", event);
-    if (inputField.value === "matias") {
-      inputField.focus();
-    }
-  });
-});
- */
-
-const scoopy = document.createElement("img");
-scoopy.src = "./assets/gifs/scoopy.gif";
-scoopy.style.width = "200px";
-
-document.body.append(scoopy);
-
-const cheatcodeInput = document.querySelector("#cheatcode");
-
-const cheatCode = [];
-document.addEventListener("keydown", (e) => {
-  cheatCode.push(e.key);
-  console.log("keydown");
-});
+document.addEventListener;
