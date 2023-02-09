3596f541ba6ab746fc13adb1a9e798039ebb05b9
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-02-09 18:49:48 +0200

    update

---
+++ src/modules/sodexo-data.js
@@ -0,0 +1,11 @@
+
+import Menu from '../mock-data/sodexo.json';
+// console.log('menu from json', Menu.courses);
+
+// Convert Menu.courses object to array and extract title_* values only
+const coursesEn = Object.values(Menu.courses).map((course) => course.title_en);
+const coursesFi = Object.values(Menu.courses).map((course) => course.title_fi);
+
+const Sodexo = {coursesEn, coursesFi};
+
+export default Sodexo;
