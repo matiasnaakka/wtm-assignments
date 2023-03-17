bf6f0118ed8c13ad87a0dc11d0223db099e8e0c2
Matias Naakka <matiasnaakka.matias5230@gmail.com>
2023-03-17 18:57:02 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('Toimii kuin junan vessa eikös niin!');
+console.log("this is my example webpack app");
+
+let counter = 0;
+const timer = setInterval(() => {
+  counter++;
+  console.log(`2 sekkaa meni ${counter++} kerran.`);
+  if (counter > 9) {
+    clearInterval(timer);
+  }
+}, 2000);
+
+console.log("suoritus jatkuu");
+
+document.addEventListener("click", (e) => {
+  console.log("dokumenttia klikattu", e);
+  clearInterval(timer);
+});
+
+const divs = document.querySelectorAll("div");
+// console.log(div);
+
+div[0].addEventListener("click", (event) => {
+  console.log("ekaa klikattu");
+});
+
+div[1].addEventListener("click", (event) => {
+  console.log("tokaa klikattu");
+});
+
+div[2].addEventListener("click", (event) => {
+  console.log("kolmatta klikattu");
+});
+
+div[0].addEventListener("click", (event) => {
+  console.log("tokaa klikattu");
+  div[1].borderColor = "100%";
+});
+
+div[6].addEventListener("click", (event) => {
+  console.log("kolmatta klikattu");
+  div[1].borderColor = "black";
+});
+
+document.addEventListener("keypress", (event) => {
+  console.log(event.key);
+  if(inputField.value === '');
+});
